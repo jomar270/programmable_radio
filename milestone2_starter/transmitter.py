677f72da7112ef1e7d3c5dcace3cd6d3cca9a56d
John Pulvera <johnpulvera@rescomp-12-214333.stanford.edu>
2013-05-08 15:53:49 -0700

    sampling

--- milestone2_starter/transmitter.py
+++ milestone2_starter/transmitter.py
@@ -37,10 +37,32 @@ class Transmitter:
 
         # convert bits to samples
         # [one] and 0
-        samples = databits_with_preamble
-        print "samples:", samples
-        print type(samples)
-        
+        # print "self.one:", self.one
+        # samples = databits_with_preamble
+        # print "samples:", samples
+        # print type(samples)
+
+        # print self.spb
+        samples_array = []
+        for databit in numpy.nditer(databits_with_preamble):
+            # print databit
+            if (databit == 1):
+                # print [self.one] * self.spb
+                # print len([self.one] * self.spb)
+                # samples_array.append([self.one] * self.spb)
+                samples_array.extend([self.one] * self.spb)
+            elif (databit == 0):
+                # print [0] * self.spb
+                # print len([0] * self.spb)
+                # samples_array.append([0] * self.spb)
+                samples_array.extend([0] * self.spb)
+            else:
+                print "error"
+        # print samples_array
+        # samples = numpy.array([])
+        samples = numpy.array(samples_array)
+        # print "samples:", samples
+
         return samples
         
 
