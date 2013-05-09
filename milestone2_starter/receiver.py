577f4681673d47bc619fa1296e5dec043a7db74f
John Pulvera <johnpulvera@dn52052k.sunet>
2013-05-08 18:00:59 -0700

    averaging samples and energy offset

--- milestone2_starter/receiver.py
+++ milestone2_starter/receiver.py
@@ -33,14 +33,45 @@ class Receiver:
         is crucial to a proper and correct synchronization w/ the xmitter.
         '''
 
+        # print "demod_samples:", demod_samples
+        # print "thresh:", thresh
+        # print "one:", one
+        # print "self.spb:", self.spb
+        # print type(demod_samples)
+        # print demod_samples.size # num samples
+        # print demod_samples.size / self.spb # actual bits
+        # print demod_samples.size / self.spb - 200 - 118 # actual data bits, minus silence and preamble
+
         '''
         First, find the first sample index where you detect energy based on the
         moving average method described in the milestone 2 description.
         '''
 
         # high-energy check procedure to get energy_offset
-        # energy_offset = # fill in the result of the high-energy check
-        energy_offset = 0
+        # for each sample, determine if 1-value is reached
+        # if so, return energy_offset of that offset
+        # else, return energy_offset of -1
+        energy_offset = -1
+        num_samples = demod_samples.size / self.spb
+        for i in range(num_samples):
+            index = i * self.spb
+            sample = demod_samples[index : index+self.spb]
+            average = self.averageSample(sample)
+
+            # print num_samples
+            # print sample
+            # print sample.size
+            # print average
+            # print index
+            print average
+
+            if average > thresh:
+                energy_offset = index
+                break
+            # break # testing
+        print "energy_offset:", energy_offset
+
+        # if energy_offset couldn't be detected
         if energy_offset < 0:
             print '*** ERROR: Could not detect any ones (so no preamble). ***'
             print '\tIncrease volume / turn on mic?'
@@ -64,6 +95,30 @@ class Receiver:
         '''
 
         return energy_offset + preamble_offset
+
+    def averageSample(self, sample):
+        '''
+        Get average of middle half of values in sample.
+        '''
+
+        window = self.spb / 2
+        # average = numpy.average(sample[self.spb: self.spb+window])
+        average = numpy.average(sample[window/2 : window/2+window])
+
+        # print average
+        # print sample
+        # print type(sample)
+        # print window
+        # print self.spb
+        # print self.spb + window
+        # print sample[self.spb : self.spb+window]
+        # print window/2
+        # print sample[window/2 : window/2+window]
+        # print len(sample[window/2 : window/2+window])
+        # print type(sample[self.spb: self.spb+window])
+
+        return average
+
         
     def demap_and_check(self, demod_samples, preamble_start):
         '''
