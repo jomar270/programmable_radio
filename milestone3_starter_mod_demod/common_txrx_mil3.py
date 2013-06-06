0259064a5f1eda611b8c75f33088fd38c9303985
John Pulvera <johnpulvera@dn0a20324a.sunet>
2013-06-05 17:06:16 -0700

    demod and lpfilter, except using cos instead of exp

--- milestone3_starter_mod_demod/common_txrx_mil3.py
+++ milestone3_starter_mod_demod/common_txrx_mil3.py
@@ -22,16 +22,56 @@ def demodulate(fc, samplerate, samples):
   '''
   A demodulator that performs quadrature demodulation
   '''
-  return 0
+
+  s = len(samples)
+  demod_samples = numpy.empty(s)
+  for n in range(s):
+
+    #change the inverse_sample to use exponential
+    inverse_sample = math.cos(2 * math.pi * fc / samplerate * n)
+    # inverse_sample = math.exp(1j * 2 * math.pi * fc / samplerate * n)
+    # print abs(1j * 2 * math.pi * fc / samplerate * n)
+    # inverse_sample = math.exp(abs(1j * 2 * math.pi * fc / samplerate * n))
+    # inverse_sample = math.e**abs(1j * 2 * math.pi * fc / samplerate * n)
+
+    demod_samples[n] = samples[n] * inverse_sample
+
+  omega_cut = math.pi * fc / samplerate
+  lpf_samples = lpfilter(demod_samples, omega_cut)
+
+  # return numpy.empty(len(lpf_samples))
+  return lpf_samples
 
 
 def lpfilter(samples_in, omega_cut):
   '''
   A low-pass filter of frequency omega_cut.
   '''
+
   # set the filter unit sample response
   L = 50
-  
+  filter_length = L * 2 + 1
+  unit_sample_response = numpy.empty(filter_length)
+  for n in range(-L, L + 1):
+    if n != 0:
+      val = math.sin(omega_cut * n) / (math.pi * n)
+    else:
+      val = omega_cut / math.pi
+    unit_sample_response[n] = val
+
   # compute the demodulated samples
-  return 0
+  s = len(samples_in)
+  lpf_samples = numpy.empty(s)
+  for n in range(s):
+    input_samples = numpy.empty(filter_length)
+    i = 0
+    for m in range(n-L, n+L+1):
+      if m < 0 or m >= s:
+        input_samples[i] = 0
+      else:
+        input_samples[i] = samples_in[m]
+      i += 1
+    lpf_samples[n] = numpy.dot(unit_sample_response, input_samples[::-1])
+
+  return numpy.absolute(lpf_samples)
 
